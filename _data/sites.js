d394992c272e5cb77fec5ba7f45ec3e0a446ee9d
Søren Birkemeyer <sb@webfactory.de>
2020-06-25 16:28:54 +0000

    Update pages, save lockfile

--- _data/sites.js
+++ _data/sites.js
@@ -1,34 +1,14 @@
 module.exports = {
-	netlify: {
-		description: "Netlify web sites",
-		options: {
-			freshChrome: "site",
-		},
-		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
-		]
-	},
 	ssg: {
 		description: "Static Site Generator web sites",
 		options: {
 			freshChrome: "site"
 		},
 		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			// "https://vuepress.vuejs.org/", // kept timing out in lighthouse
-			"https://gridsome.org/",
+			"https://annualbeta.com/",
+			"https://fantasybuchtipps.de/",
+			"https://www.jourmany.de/",
+			"https://sciencefictionbuchtipps.de/",
 		]
 	}
 };
